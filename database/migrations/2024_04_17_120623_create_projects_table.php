8494de33589da33dba4d0c72975d1a4334f2654c
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-04-18 13:54:37 +0200

    🗃️ Add a new table named "project_user".

--- database/migrations/2024_04_17_120623_create_projects_table.php
+++ database/migrations/2024_04_17_120623_create_projects_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +22,15 @@ return new class extends Migration
                 $table->timestamps();
             });
         }
+
+        
+        if (!Schema::hasTable('projects')) {
+            Schema::create('project_user', function (Blueprint $table) {
+                $table->foreignIdFor(Project::class)->constrained()->cascadeOnDelete();
+                $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+                $table->primary(['project_id', 'user_id']);
+            });
+        }
     }
 
     /**
@@ -27,6 +38,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('project_user');
         Schema::dropIfExists('projects');
     }
 };
