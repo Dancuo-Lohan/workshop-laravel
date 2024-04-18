279ed487e264634773610987384bcb883fd4dd4f
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-04-18 10:43:56 +0200

    :bug: Rename task to tasks and use the model Task.

--- database/migrations/2024_04_17_120712_create_tasks_table.php
+++ database/migrations/2024_04_17_120712_create_tasks_table.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Models\Task as Task;
 use App\Models\User;
-use Illuminate\Console\View\Components\Task;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (!Schema::hasTable('task')) {
+        if (!Schema::hasTable('tasks')) {
             Schema::create('tasks', function (Blueprint $table) {
                 $table->id();
                 $table->string('name');
