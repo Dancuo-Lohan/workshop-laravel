4d65d9bfaeb17c6a806ec62f989c0d1758f0780a
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-04-19 11:51:30 +0200

    :lipstick: :speech_balloon: Update task show view.

--- resources/views/administrator/task/show.blade.php
+++ resources/views/administrator/task/show.blade.php
@@ -7,28 +7,38 @@
         <div class="card">
             <div class="card-header">
                 <h5 class="card-title">{{ $task->name }}</h5>
-                @if ($task->tag)
-                    <span class="badge bg-dark">{{ $task->tag->label }}</span>
-                @else
-                    <span class="badge bg-secondary">Aucune catégorie</span>
-                @endif
             </div>
-            <div class="card-body">
-                <p class="card-text"> <strong>Developers : </strong>
-                <ul>
-                    @foreach ($task->developers as $developer)
-                        <li>{{ $developer->name }}</li>
-                    @endforeach
-                </ul>
-                </p>
-                <p class="card-text"> <strong>Managers : </strong>
-                <ul>
-                    @foreach ($task->projectManagers as $manager)
-                        <li>{{ $manager->name }}</li>
-                    @endforeach
-                </ul>
-                </p>
-                <p class="card-text"> <strong>Description : </strong>{!! $task->description !!}</p>
+            <div class="card w-75 mx-auto mt-4">
+                <div class="card-header">
+                    Managers
+                </div>
+                <div class="card-body">
+                    <ul>
+                        @foreach ($task->projectManagers as $projectManager)
+                            <li><a href="{{ route('administrator.projectManager.show', ['projectManager' => $projectManager]) }}">{{ $projectManager->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="card w-75 mx-auto mt-4">
+                <div class="card-header">
+                    Developers
+                </div>
+                <div class="card-body">
+                    <ul>
+                        @foreach ($task->developers as $developer)
+                            <li><a href="{{ route('administrator.developer.show', ['developer' => $developer]) }}">{{ $developer->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="card w-75 mx-auto my-4">
+                <div class="card-header">
+                    Description
+                </div>
+                <div class="card-body">
+                    <p class="card-text"> <strong>Description : </strong>{!! $task->description !!}</p>
+                </div>
             </div>
             <div class="card-footer text-muted">
                 Created on {{ $task->created_at->format('d/m/Y') }}
