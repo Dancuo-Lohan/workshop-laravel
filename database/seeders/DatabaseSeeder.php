45a332d5806c2e330bce348915a4f6da3a4b0062
Aubrian <aubrian.duhayon@mail-ecv.fr>
2024-04-19 13:25:06 +0200

    :sparkles: Add new defaults accounts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,5 @@ class DatabaseSeeder extends Seeder
             StatusTagSeeder::class,
             TaskTagSeeder::class,
         ]);
-
-        \App\Models\Task::factory(10)->create();
     }
 }
