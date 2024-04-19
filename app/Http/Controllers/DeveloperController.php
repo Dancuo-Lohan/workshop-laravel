43a82594ec78d5e21133e59d964927d240386097
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-04-19 09:56:52 +0200

    :sparkles: Add new functions for the dev controller.

--- app/Http/Controllers/DeveloperController.php
+++ app/Http/Controllers/DeveloperController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Models\Task;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class DeveloperController extends Controller
@@ -10,6 +14,23 @@ class DeveloperController extends Controller
     public function index(): View
     {
         return view('developer.index', [
+            'developer' => Auth::user()
+        ]);
+    }
+
+    public function task(Task $task): RedirectResponse | View
+    {
+        return view('developer.task', [
+            'developer' => Auth::user(),
+            'task' => $task
+        ]);
+    }
+
+    public function project(Project $project): RedirectResponse | View
+    {
+        return view('developer.project', [
+            'developer' => Auth::user(),
+            'project' => $project
         ]);
     }
 }
